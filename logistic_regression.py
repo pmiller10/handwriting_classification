2aff2b00bf6012adbf9614f41f22e4baadfb005a
Paul Miller <pmiller987@gmail.com>
2013-03-22 14:24:53 +0000

    Test neural net with different parameters

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,16 +1,17 @@
-from sklearn import metrics
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Lasso
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.naive_bayes import GaussianNB
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.naive_bayes import MultinomialNB
+import sys
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import Lasso
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import ExtraTreesClassifier
+#from sklearn.neighbors import KNeighborsRegressor
+#from sklearn.ensemble import GradientBoostingRegressor
+#from sklearn.naive_bayes import GaussianNB
+#from sklearn.naive_bayes import BernoulliNB
+#from sklearn.naive_bayes import MultinomialNB
 from numpy import array
 import time
+import pickle
 
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.datasets import SupervisedDataSet
@@ -20,7 +21,21 @@ from pybrain.structure import SigmoidLayer
 from image import Image
 from log_loss import log_loss
 
-start = time.time()
+def write(name, out):
+	f = open(name, 'w')
+	f.write(str(out))
+	f.close()
+
+def save(name, out):
+	f = open(name, 'w')
+	pickle.dump(out, f)
+	f.close()
+
+def load(name):
+	f = open(name, 'r')
+	new_net = pickle.load(f)
+	return new_net
+
 
 data = Image.data()
 genders = Image.genders()
@@ -30,21 +45,23 @@ train_data, train_genders = data[:half], genders[:half]
 test_data, test_genders = data[half:], genders[half:]
 
 
-net = buildNetwork(len(data[0]), 50, 5, 1, hiddenclass=SigmoidLayer, outclass=SigmoidLayer)
+net = buildNetwork(len(data[0]), 20, 5, 1, hiddenclass=SigmoidLayer, outclass=SigmoidLayer)
 ds = SupervisedDataSet(len(data[0]), 1)
 for i,d in enumerate(train_data):
-	t = train_genders[i]
-	ds.addSample(d, t)
+	if i < 500:
+		t = train_genders[i]
+		ds.addSample(d, t)
 
+start = time.time()
 trainer = BackpropTrainer(net, ds)
-for i in range(10):
+for i in range(500):
 	trainer.train()
 
 
-model1 = LogisticRegression()
-model2 = ExtraTreesClassifier(n_estimators=30)
-model3 = GradientBoostingRegressor()
-model4 = KNeighborsRegressor(n_neighbors=20)
+#model1 = LogisticRegression()
+#model2 = ExtraTreesClassifier(n_estimators=30)
+#model3 = GradientBoostingRegressor()
+#model4 = KNeighborsRegressor(n_neighbors=20)
 #model = GaussianNB()
 #model = RandomForestClassifier(n_estimators=30)
 
@@ -78,7 +95,8 @@ for p in all_preds[1:]:
 
 random = [0.5] * len(preds)
 
-print log_loss(merged, test_genders)
-print log_loss(random, test_genders)
-
-print time.time() - start
+print "Time: ", time.time() - start
+print "Score: ", log_loss(merged, test_genders)
+print "Random score: ", log_loss(random, test_genders)
+save('20_and_5_hidden_500_epoch.txt', net)
+write('20_and_5_hidden_500_epoch_score.txt', log_loss(merged, test_genders))
