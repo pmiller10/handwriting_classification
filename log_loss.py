4a1f2ac5968c4bdcf74f42c7138963e497aefb5d
Paul Miller <pmiller987@gmail.com>
2013-04-14 23:20:45 +0000

    Update runner

--- log_loss.py
+++ log_loss.py
@@ -3,13 +3,15 @@ from math import log
 
 
 def log_loss(predicted, target):
-	if len(predicted) != len(target):
-		print 'lengths not equal!'
-		return
-	#print target
-	target = [float(x) for x in target]   # make sure all float values
-	#print target
-	#print predicted 
-	predicted = [min([max([x,1e-15]),1-1e-15]) for x in predicted]  # within (0,1) interval
-	#print predicted 
-	return -(1.0/len(target))*sum([target[i]*log(predicted[i]) + (1.0-target[i])*log(1.0-predicted[i]) for i in xrange(len(target))])
+    if len(predicted) != len(target):
+        print len(predicted)
+        print len(target)
+        print 'lengths not equal!'
+        return
+    #print target
+    target = [float(x) for x in target]   # make sure all float values
+    #print target
+    #print predicted 
+    predicted = [min([max([x,1e-15]),1-1e-15]) for x in predicted]  # within (0,1) interval
+    #print predicted 
+    return -(1.0/len(target))*sum([target[i]*log(predicted[i]) + (1.0-target[i])*log(1.0-predicted[i]) for i in xrange(len(target))])
--- runner.py
+++ runner.py
@@ -5,21 +5,19 @@ from log_loss import log_loss
 from postprocess import PostProcess
 
 genders = Image.genders()
-d = Image.data()
+d, _ = Image.data()
 matrix = Preprocess.to_matrix(d)
 print matrix.shape
 matrix = Preprocess.remove_constants(matrix)
 print matrix.shape
-#matrix = Preprocess.scale(matrix)
-#matrix = Preprocess.polynomial(matrix, 5)
+matrix = Preprocess.scale(matrix)
+matrix = Preprocess.polynomial(matrix, 3)
 matrix = Preprocess.scale(matrix)
 print matrix.shape
 matrix = matrix.tolist()
 half = len(matrix)/2
 train, cv = matrix[:half], matrix[half:]
 train_genders, cv_genders = genders[:half], genders[half:]
+cv_genders = cv_genders[0::4]
 preds = Classifier.ensemble_preds(train, train_genders, cv)
 print "Score: ", log_loss(preds, cv_genders)
-
-# for creating submission file
-PostProcess(ids, preds)
